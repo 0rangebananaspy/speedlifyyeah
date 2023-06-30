cbb5042d2eb1734fb3c868087d0d832ed44e3cef
Techies <94751583+0rangebananaspy@users.noreply.github.com>
2023-06-30 23:45:16 +0700

    Update universitas.js

--- _data/sites/universitas.js
+++ _data/sites/universitas.js
@@ -4,7 +4,10 @@ module.exports = {
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	//skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 11 + 30, // 11h, 30m
+		runs: 1,
+		frequency: 1, // (in minutes)
+		//frequency: 60 * 23, // 24 hours
+		//frequency: 60 * 11 + 30, // 11h, 30m
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
