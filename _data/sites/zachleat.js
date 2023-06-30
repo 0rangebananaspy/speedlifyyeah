466e43a6e02e4b8e3419e97ea1274aa870195f1d
Techies <94751583+0rangebananaspy@users.noreply.github.com>
2023-06-30 23:05:42 +0700

    Update zachleat.js

--- _data/sites/zachleat.js
+++ _data/sites/zachleat.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: "zachleat.com", // optional, falls back to object key
-	description: "Zach’s Personal web site",
+	name: "Monitoring Performa Website UPT dan Lembaga", // optional, falls back to object key
+	description: "Monitoring performa website UPT dan lembaga",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
@@ -13,17 +13,29 @@ module.exports = {
 		freshChrome: "site"
 	},
 	urls: [
-		"https://www.zachleat.com/",
-		"https://www.zachleat.com/web/",
-		"https://www.zachleat.com/web/about/",
-		"https://www.zachleat.com/web/fonts/",
-		"https://www.zachleat.com/web/eleventy/",
-		"https://www.zachleat.com/resume/",
-		"https://www.zachleat.com/twitter/",
+		"https://uptti.unej.ac.id/",
+		"https://kearsipan.unej.ac.id/",
+		"https://penerbitan.unej.ac.id/",
+		"https://uptbahasa.unej.ac.id/",
+		"https://perpustakaan.unej.ac.id/",
+		"https://ltsit-cdast.unej.ac.id/",
+		"https://agroteknopark.unej.ac.id/",
 		// Popular Posts
-		"https://www.zachleat.com/web/lighthouse-in-footer/",
-		"https://www.zachleat.com/web/speedlify/",
-		"https://www.zachleat.com/web/comprehensive-webfonts/",
-		"https://www.zachleat.com/web/google-fonts-display/",
+		"https://spi.unej.ac.id/",
+		"https://lp2m.unej.ac.id/",
+		"https://lp3m.unej.ac.id/",
+		"https://chrm2.unej.ac.id/",
+		"https://cgant.unej.ac.id/",
+		"https://c-rissh.unej.ac.id/",
+		"https://cgs.unej.ac.id/",
+		"https://pusatstudigender.unej.ac.id/",
+		"https://rsgm.unej.ac.id/",
+		"https://umc.unej.ac.id/",
+		"https://ppid.unej.ac.id/",
+		"https://mmp.unej.ac.id/",
+		"https://oailib.unej.ac.id/",
+		"https://library.unej.ac.id/",
+		"https://jurnal.unej.ac.id/",
+		"https://mbkm.unej.ac.id/"
 	]
 };
