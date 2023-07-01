7f6232151e19937fabade2a7e7fb78d4de57169f
Techies <94751583+0rangebananaspy@users.noreply.github.com>
2023-07-01 11:22:51 +0700

    Update fakultas.js

--- _data/sites/fakultas.js
+++ _data/sites/fakultas.js
@@ -5,9 +5,9 @@ module.exports = {
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	//skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-  runs: 1,
-		frequency: 1, // (in minutes)
-		//frequency: 60 * 23, // 24 hours
+                runs: 10,
+		//frequency: 1, // (in minutes)
+		frequency: 60 * 23, // 24 hours
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
