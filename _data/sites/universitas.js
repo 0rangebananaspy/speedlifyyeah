c81dd3b147e47ab5a28c5ce2fe6b5367cecd45d4
Techies <94751583+0rangebananaspy@users.noreply.github.com>
2023-07-01 08:41:40 +0700

    Update universitas.js

--- _data/sites/universitas.js
+++ _data/sites/universitas.js
@@ -2,7 +2,7 @@ module.exports = {
 	description: "Monitoring Performa Website Universitas",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
-	//skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
 		runs: 1,
 		frequency: 1, // (in minutes)
