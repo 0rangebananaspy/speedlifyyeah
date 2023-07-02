dedbf15207032cad0edb05d5bdda43162419d80b
Techies <94751583+0rangebananaspy@users.noreply.github.com>
2023-07-02 09:57:00 +0700

    Update unit.js

--- _data/sites/unit.js
+++ _data/sites/unit.js
@@ -3,10 +3,10 @@ module.exports = {
 	description: "Monitoring Performa Website UPT dan Lembaga",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	//skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
 		runs: 1,
-		frequency: 1, // (in minutes)
+		frequency: 60, // (in minutes)
 		//frequency: 60 * 23, // 24 hours
 		//frequency: 60 * 23, // 23 hours
 		// Use "run" if the sites don’t share assets on the same origin
