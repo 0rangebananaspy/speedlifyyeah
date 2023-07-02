78d65c9f7d93b39d3c6987e9ad8b90b657fdf157
Techies <94751583+0rangebananaspy@users.noreply.github.com>
2023-07-02 09:57:21 +0700

    Update universitas.js

--- _data/sites/universitas.js
+++ _data/sites/universitas.js
@@ -2,10 +2,10 @@ module.exports = {
 	description: "Monitoring Performa Website Universitas",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	//skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		runs: 1,
-		frequency: 1, // (in minutes)
+		runs: 10,
+		frequency: 60, // (in minutes)
 		//frequency: 60 * 23, // 24 hours
 		//frequency: 60 * 11 + 30, // 11h, 30m
 		// Use "run" if the sites don’t share assets on the same origin
